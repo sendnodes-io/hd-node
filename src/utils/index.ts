8ea0de237d66c226c60458f9b234e4faf3987302
rBurgett <ryan@burgettdev.net>
2023-08-28 00:55:22 -0400

    Replace addressFromPublickey with local implementation to reduce depependency overhead

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,12 @@ import aesjs from "aes-js"
 export function computeFingerprint(publicKey: string): string {
   return hexDataSlice(ripemd160(sha256(aesjs.utils.hex.toBytes(publicKey))), 0, 4)
 }
+
+/**
+ * Converts a public key to an address synchronously
+ * Adapted from the official async implementation found here:
+ * https://github.com/pokt-foundation/pocket-js/blob/v2.1.1/packages/utils/src/addr-from-pubkey.ts#L24
+ */
+export function addressFromPublickey(publicKey: Uint8Array): string {
+  return sha256(publicKey).slice(2, 42);
+}
